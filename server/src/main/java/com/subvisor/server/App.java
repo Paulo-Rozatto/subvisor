795e0fa76ba9346bd3ace02a507400110b0ec119
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2023-10-06 10:57:10 -0300

    Creating data dir in home and serving datasets

--- server/src/main/java/com/subvisor/server/App.java
+++ server/src/main/java/com/subvisor/server/App.java
@@ -1,12 +1,45 @@
 package com.subvisor.server;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.val;
 
 @SpringBootApplication
-public class App {
+public class App implements WebMvcConfigurer {
+	private static String DATA_DIR_PATH;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		val datasetPath = Paths.get(DATA_DIR_PATH, "datasets").toString();
+
+		System.out.println(datasetPath);
+		registry.addResourceHandler("/datasets/**")
+				.addResourceLocations("file:" + datasetPath + "/")
+				.setCachePeriod(0);
+	}
+
+	private static String getOrCreateDataDirs() {
+		val homePath = System.getProperty("user.home");
+		val rootDataDir = Paths.get(homePath, "subvisor").toString();
+		val datasetsDir = Paths.get(rootDataDir, "datasets");
+
+		try {
+			Files.createDirectories(datasetsDir);
+			return rootDataDir;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public static void main(String[] args) {
+		DATA_DIR_PATH = getOrCreateDataDirs();
 		SpringApplication.run(App.class, args);
 	}
 
