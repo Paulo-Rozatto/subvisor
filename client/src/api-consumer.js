def01c1ce8158a43394c611412a01e446bca4ad1
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2023-10-09 17:43:08 -0300

    Add dataset list modal

--- client/src/api-consumer.js
+++ client/src/api-consumer.js
@@ -6,6 +6,8 @@ import {
     getCurrentClass
 } from './app.js';
 
+const API_URL = 'http://localhost:8080/api';
+
 export async function boxbuttonHandler() {
     if (getCurrentClass() !== CLASSES.BOX) {
         setSelectedPoints(CLASSES.BOX);
@@ -15,11 +17,20 @@ export async function boxbuttonHandler() {
     const { imagePath, points } = getImagePath();
     const params = new URLSearchParams({ path: imagePath, points });
     const response = await fetch(
-        `http://localhost:8080/api/nn/test?${params}`,
-        { method: "GET", mode: "cors" }
+        `${API_URL}/nn/test?${params}`,
+        { method: 'GET', mode: 'cors' }
     );
     const json = await response.json();
     const pointsArray = JSON.parse(json.points) || [];
     const resultPoints = pointsArray.map((point) => ({ x: point[0], y: point[1] }));
     setLeaftPoints(resultPoints);
+}
+
+export async function fetchDatasetList() {
+    const response = await fetch(
+        `${API_URL}/datasets/list`,
+        { method: 'GET', mode: 'cors' }
+    );
+    const json = await response.json();
+    return json;
 }
