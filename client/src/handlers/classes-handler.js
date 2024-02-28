19c66008fd3c8b95c407337807fabf92d1e07950
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2024-02-28 20:36:54 -0300

    Add edit class feature

--- client/src/handlers/classes-handler.js
+++ client/src/handlers/classes-handler.js
@@ -2,8 +2,56 @@ import { annotateLeaf, saveXml } from "../api-consumer";
 import { currentPath, selected } from "./dataset-load-handler";
 import CLASSES from "../classes.json";
 import { IMAGE_MAP } from "../app/app";
+import { modalToggle } from "../utils";
 import { DefaultParser as parser } from "../app/default-parser";
 import { Renderer as renderer } from "../app/renderer";
+
+let current = "";
+
+const classesDisplay = document.querySelector("#classes-display");
+const classesSelect = document.querySelector("#classes-select");
+const classesModal = document.querySelector("#classes-modal");
+const saveClassesButton = document.querySelector("#save-classes");
+
+function setCurrent(newClass) {
+    if (newClass === "") {
+        classesDisplay.innerText = "-";
+        current = "";
+    }
+
+    if (!CLASSES[newClass]) {
+        return;
+    }
+
+    current = newClass;
+    classesDisplay.innerText = newClass;
+}
+
+function openClassesModal() {
+    if (!current) {
+        return;
+    }
+
+    classesSelect.value = current;
+    modalToggle(classesModal);
+}
+
+function swithClass(event) {
+    event.preventDefault();
+
+    if (classesSelect.value === current || !renderer.focused) {
+        return;
+    }
+
+    setCurrent(classesSelect.value);
+
+    if (current) {
+        renderer.focused.class = current;
+        renderer.render();
+    }
+
+    modalToggle(classesModal);
+}
 
 async function predictAnnotation(points) {
     const fileName = selected.innerText + ".jpg";
@@ -35,13 +83,28 @@ async function predictAnnotation(points) {
     saveXml(currentPath, "leaf", fileName.replace(".jpg", ".xml"), xml);
 }
 
-document.querySelector("#predict-button").addEventListener("click", () => {
-    if (renderer.showRoi) {
-        predictAnnotation(renderer.roi.points);
+saveClassesButton.addEventListener("click", swithClass);
+classesDisplay.parentElement.addEventListener("click", openClassesModal);
+
+(() => {
+    const fragment = new DocumentFragment();
+    for (const _class in CLASSES) {
+        const element = document.createElement("option");
+        element.innerText = _class;
+        element.value = _class;
+        fragment.append(element);
     }
-});
+    classesSelect.append(fragment);
+})();
 
 export const ClassesHandler = {
     ...CLASSES,
     predictAnnotation,
+
+    get current() {
+        return current;
+    },
+    set current(newClass) {
+        setCurrent(newClass);
+    },
 };
