1756b7e81b5bcbe78de4c6c59e38ec36aa3c461a
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2024-03-07 20:44:59 -0300

    Add cpu ONNX runtime for points prompts

---
+++ server/src/main/java/com/subvisor/server/neuralnetwork/SamHq.java
@@ -0,0 +1,168 @@
+package com.subvisor.server.neuralnetwork;
+
+import ai.onnxruntime.OnnxTensor;
+import ai.onnxruntime.OrtEnvironment;
+import ai.onnxruntime.OrtException;
+import ai.onnxruntime.OrtSession;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Map;
+
+import static org.opencv.core.CvType.CV_32F;
+import static org.opencv.core.CvType.CV_8UC1;
+
+public class SamHq {
+    final private OrtEnvironment env;
+    final private OrtSession session;
+    final private OrtSession decoderSession;
+
+    public SamHq() {
+        try {
+            byte[] encoder = Objects.requireNonNull(getClass().getResourceAsStream("/models/encoder.onnx")).readAllBytes();
+            byte[] decoder = Objects.requireNonNull(getClass().getResourceAsStream("/models/decoder.onnx")).readAllBytes();
+
+            env = OrtEnvironment.getEnvironment();
+            session = env.createSession(encoder, new OrtSession.SessionOptions());
+            decoderSession = env.createSession(decoder, new OrtSession.SessionOptions());
+        } catch (IOException | NullPointerException | OrtException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String run(String imagePath, String points, String labels) {
+        Mat imageMat = Imgcodecs.imread(imagePath);
+        float[] pts = str2array(points + ",0,0");
+        float[] lbls = str2array(labels + ",-1");
+
+        float[] originalSize = {imageMat.rows(), imageMat.cols()};
+        float scale = 1024 / (float) Math.max(imageMat.rows(), imageMat.cols());
+        resize(imageMat, scale);
+
+        try {
+            float[] inputSize = {imageMat.rows(), imageMat.cols()};
+            OrtSession.Result embeddings = encode(imageMat);
+            OrtSession.Result result = decode(embeddings, pts, scale, lbls, inputSize);
+
+            Mat mask = new Mat((int) inputSize[0], (int) inputSize[1], CV_8UC1);
+            float[][] output = ((float[][][][]) result.get("masks").get().getValue())[0][0];
+
+            int rows = (int) inputSize[0];
+            int cols = (int) inputSize[1];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    int value = output[i][j] > 0 ? 1 : 0;
+                    mask.put(i, j, value);
+                }
+            }
+
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            double maxVal = 0;
+            int maxValIdx = 0;
+            for (int contourIdx = 0; contourIdx < contours.size(); contourIdx++) {
+                double contourArea = Imgproc.contourArea(contours.get(contourIdx));
+                if (maxVal < contourArea) {
+                    maxVal = contourArea;
+                    maxValIdx = contourIdx;
+                }
+            }
+
+            MatOfPoint2f polyPoints = new MatOfPoint2f(contours.get(maxValIdx).toArray());
+            double peri = Imgproc.arcLength(polyPoints, true);
+            Imgproc.approxPolyDP(polyPoints, polyPoints, 0.0006 * peri, true);
+
+            StringBuilder stringArray = new StringBuilder("[");
+            String x, y;
+            double xRatio = originalSize[0] / inputSize[0];
+            double yRatio = originalSize[1] / inputSize[1];
+            for (Point p : polyPoints.toArray()) {
+                x = String.valueOf(p.x * xRatio);
+                y = String.valueOf(p.y * yRatio);
+                stringArray.append(String.format("[%s, %s],", x, y));
+            }
+            stringArray.replace(stringArray.length() - 1, stringArray.length(), "]");
+
+            return stringArray.toString();
+
+        } catch (OrtException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private OrtSession.Result encode(Mat imageMat) throws OrtException {
+        float[] floatData = new float[imageMat.rows() * imageMat.cols() * imageMat.channels()];
+        imageMat.convertTo(imageMat, CV_32F);
+        imageMat.get(0, 0, floatData);
+
+        // Create OnnxTensor
+        long[] shape = {imageMat.rows(), imageMat.cols(), imageMat.channels()};
+        OnnxTensor imageTensor = OnnxTensor.createTensor(env, FloatBuffer.wrap(floatData), shape);
+
+        return session.run(Collections.singletonMap("input_image", imageTensor));
+    }
+
+    private OrtSession.Result decode(OrtSession.Result result, float[] points, float scale, float[] labels, float[] inputSize) throws OrtException {
+        float[] mask = new float[256 * 256];
+        float[] hasMask = {0.0f};
+
+        long[] pointsShape = {1, points.length / 2, 2};
+        long[] labelsShape = {1, pointsShape[1]};
+        long[] maskShape = {1, 1, 256, 256};
+        long[] hasMaskShape = {1};
+        long[] sizeShape = {2};
+
+
+        for (int i = 0; i < points.length; i++) {
+            points[i] *= scale;
+        }
+
+        OnnxTensor imageEmbeddings = (OnnxTensor) result.get("image_embeddings").get();
+        OnnxTensor intermEmbeddings = (OnnxTensor) result.get("interm_embeddings").get();
+        OnnxTensor pointsCoords = OnnxTensor.createTensor(env, FloatBuffer.wrap(points), pointsShape);
+        OnnxTensor pointsLabels = OnnxTensor.createTensor(env, FloatBuffer.wrap(labels), labelsShape);
+        OnnxTensor maskInput = OnnxTensor.createTensor(env, FloatBuffer.wrap(mask), maskShape);
+        OnnxTensor hasMaskInput = OnnxTensor.createTensor(env, FloatBuffer.wrap(hasMask), hasMaskShape);
+        OnnxTensor origImSize = OnnxTensor.createTensor(env, FloatBuffer.wrap(inputSize), sizeShape);
+
+        return decoderSession.run(Map.of(
+                "image_embeddings", imageEmbeddings,
+                "interm_embeddings", intermEmbeddings,
+                "point_coords", pointsCoords,
+                "point_labels", pointsLabels,
+                "mask_input", maskInput,
+                "has_mask_input", hasMaskInput,
+                "orig_im_size", origImSize
+        ));
+
+    }
+
+    private float[] str2array(String dataPoints) {
+        String[] stringPts = dataPoints.split(",");
+        float[] pts = new float[stringPts.length];
+
+        for (int i = 0; i < stringPts.length; i++) {
+            pts[i] = Float.parseFloat(stringPts[i]);
+        }
+        return pts;
+    }
+
+    private void resize(Mat imageMat, float scale) {
+        Size newSize = new Size(
+                (int) (imageMat.cols() * scale + 0.5),
+                (int) (imageMat.rows() * scale + 0.5)
+        );
+        int interpolation = scale < 1 ? Imgproc.INTER_AREA : Imgproc.INTER_CUBIC;
+        Imgproc.resize(imageMat, imageMat, newSize, 0, 0, interpolation);
+    }
+}
